b22317e426ca58f7c40a579da11437d0fd388fec
NIR3X <90041219+NIR3X@users.noreply.github.com>
2024-02-01 18:36:04 +0000

    .

--- sse2/FastXor.h
+++ sse2/FastXor.h
@@ -9,19 +9,19 @@ class CFastXor {
 public:
 	static void Xor(uint8_t* data, size_t dataSize, const uint8_t* key, size_t keySize);
 
-	static __forceinline void Xor(std::vector<uint8_t>& data, const std::vector<uint8_t>& key) {
+	static inline void Xor(std::vector<uint8_t>& data, const std::vector<uint8_t>& key) {
 		Xor(data.data(), data.size(), key.data(), key.size());
 	}
 
-	static __forceinline void Xor(std::string& data, const std::vector<uint8_t>& key) {
+	static inline void Xor(std::string& data, const std::vector<uint8_t>& key) {
 		Xor((uint8_t*)data.data(), data.size(), key.data(), key.size());
 	}
 	
-	static __forceinline void Xor(std::string& data, const std::string& key) {
+	static inline void Xor(std::string& data, const std::string& key) {
 		Xor((uint8_t*)data.data(), data.size(), (const uint8_t*)key.data(), key.size());
 	}
 
-	static __forceinline void Xor(std::vector<uint8_t>& data, const std::string& key) {
+	static inline void Xor(std::vector<uint8_t>& data, const std::string& key) {
 		Xor(data.data(), data.size(), (const uint8_t*)key.data(), key.size());
 	}
 };
